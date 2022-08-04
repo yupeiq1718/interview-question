b9257b1ac5ada73a1374d748cfc68ecd715c5ee6
PeiYu <yupeiq1718@gmail.com>
2022-08-04 14:19:42 +0800

    docs: modify question two description

--- src/views/QuestionTwo.vue
+++ src/views/QuestionTwo.vue
@@ -4,7 +4,7 @@
 <template>
   <div class="w-full">
     <!-- 使用 CSS/SASS 達成 RWD -->
-    <!-- 隨畫面縮放都能維持 16:9 的長寬比 -->
+    <!-- 隨畫面縮放都能維持寬度滿版和 16:9 的長寬比 -->
     <iframe
       src="https://www.youtube.com/embed/hFQR-JaGRjE"
       title="YouTube video player"
