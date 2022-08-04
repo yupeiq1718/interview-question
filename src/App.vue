ce738a21f15e2d4cf321bab7edd47549aaabe262
PeiYu <yupeiq1718@gmail.com>
2022-08-04 15:18:42 +0800

    feat: add header

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,32 @@
 <template>
-  <router-view />
+  <header class="m-4">
+    <router-link
+      class="p-1 m-1 border-2 border-black bg-white hover:bg-black text-black hover:text-white"
+      to="/question-one"
+    >
+      Q1
+    </router-link>
+    <router-link
+      class="p-1 m-1 border-2 border-black bg-white hover:bg-black text-black hover:text-white"
+      to="/question-two"
+    >
+      Q2
+    </router-link>
+    <router-link
+      class="p-1 m-1 border-2 border-black bg-white hover:bg-black text-black hover:text-white"
+      to="/question-three"
+    >
+      Q3
+    </router-link>
+    <hr class="my-4 border-2 border-black">
+  </header>
+  <main class="my-2">
+    <router-view />
+  </main>
 </template>
 
 <style>
 #app {
-  margin-top: 60px;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   text-align: center;
   color: #2c3e50;
