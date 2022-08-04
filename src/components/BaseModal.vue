a1e5ca5e2eec9f6d33e1d9b411cc33a1972596bd
PeiYu <yupeiq1718@osensetech.com>
2022-08-04 13:48:54 +0800

    feat: modify BaseModal

--- src/components/BaseModal.vue
+++ src/components/BaseModal.vue
@@ -1,10 +1,11 @@
 <template>
-  <div class="bg-black w-full h-full fixed top-0 left-0 opacity-50 flex justify-center items-center">
+  <div class="w-full h-full fixed top-0 left-0 flex justify-center items-center pointer-events-none">
     <!-- 需要可以關閉 Modal -->
-    <button class="absolute right-[20px] top-[20px] text-white hover:text-black">
-      X
-    </button>
-    <div class="w-1/2 h-2/3 bg-white px-4 py-2">
+
+    <div class="relative w-1/2 h-2/3 bg-white px-4 py-2 border-2 border-black pointer-events-auto">
+      <button class="absolute right-[20px] bottom-[20px] text-black">
+        X
+      </button>
       <!-- Content -->
       <!-- 內容需要可以自定義 -->
     </div>
